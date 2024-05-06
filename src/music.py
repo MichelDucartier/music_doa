5549a5caa96b83ad802275b73abe70f9076f2557
Michael ZHANG <michael.zhang@epfl.ch>
2024-05-07 00:10:30 +0200

    Fix conjugate problem + add correlated case?

--- src/music.py
+++ src/music.py
@@ -6,7 +6,7 @@ import scipy
 
 SOUND_SPEED = 343
     
-def music(samples, n_sources, mic_coords, main_frequency):
+def music(samples, n_sources, mic_coords, main_frequency, correlated=False):
     M = len(samples)
     
     samples = (samples.T - samples.mean(axis=1).T).T
@@ -14,7 +14,12 @@ def music(samples, n_sources, mic_coords, main_frequency):
     print('Samples shape:', samples.shape)
     covariance = samples @ samples.conj().T / M
     print("Covariance shape:", covariance.shape)
-        
+    
+    if correlated:
+        J = np.flip(np.eye(mic_coords.shape[1]), axis=1)
+        covariance = covariance + (J @ covariance.conj() @ J)
+
+
     eigenvalues, eigenvectors = np.linalg.eigh(covariance)
     
     # Decompose signal and noise
@@ -31,7 +36,9 @@ def compute_stfd(samples, nperseg, normalized_freq_range):
     # Iterate over every microphone    
     for samples_channel in samples:
         freq, time_intervals, stft = scipy.signal.stft(samples_channel, nperseg=nperseg, return_onesided=True)
-        selected_freq_id = np.argwhere((normalized_freq_range[0] <= freq) & (freq <= normalized_freq_range[1])).flatten()
+        
+        low_freq_mask = (normalized_freq_range[0] <= freq) & (freq <= normalized_freq_range[1])        
+        selected_freq_id = np.argwhere(low_freq_mask).flatten()
         stft = stft[selected_freq_id, :]
         
         sources_stft.append(stft)
@@ -40,7 +47,7 @@ def compute_stfd(samples, nperseg, normalized_freq_range):
     sources_stft = np.array(sources_stft).transpose((1, 2, 0))
     
     print("Shape of sources STFT:", sources_stft.shape)
-    stfd_matrices = np.einsum('ijk,ijl->ijkl', sources_stft, sources_stft.conj())
+    stfd_matrices = np.einsum('ijk,ijl->ijkl', sources_stft.conj(), sources_stft)
     
     stfd = stfd_matrices.mean(axis=(0, 1))
     print("STFD shape:", stfd.shape)
@@ -48,9 +55,11 @@ def compute_stfd(samples, nperseg, normalized_freq_range):
     return stfd
 
 
-def music_with_frequency(samples, n_sources, fs, mics_coords, segment_duration=0.5, freq_range=None):
+def music_with_frequency(samples, n_sources, fs, mics_coords, segment_duration=0.5, freq_range=None,
+                         correlated=False):
     if freq_range is None:
         freq_range = [0, fs]
+    
     freq_range = np.array(freq_range)
     normalized_freq_range = freq_range / fs
     
@@ -59,6 +68,10 @@ def music_with_frequency(samples, n_sources, fs, mics_coords, segment_duration=0
     
     samples = (samples.T - samples.mean(axis=1).T).T
     stfd = compute_stfd(samples, nperseg, normalized_freq_range)
+
+    if correlated:
+        J = np.flip(np.eye(mics_coords.shape[1]), axis=1)
+        stfd = stfd + np.dot(J, np.dot(stfd.conj(), J))
     
     eigenvalues, eigenvectors = np.linalg.eigh(stfd)
     
@@ -80,7 +93,7 @@ def general_spectrum_function(noise_eigenvectors, mic_locations, main_frequency)
     
     def helper(theta):
         a = np.array([np.cos(theta), np.sin(theta)])
-        atheta = np.exp(2j * np.pi / wavelength * np.dot(mic_locations, a))
+        atheta = np.exp(-2j * np.pi / wavelength * np.dot(mic_locations, a))
         temp = atheta.conj() @ noise_eigenvectors
         
         return 1 / np.linalg.norm(temp)**2
