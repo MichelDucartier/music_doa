f850b06a161756f025c3c745ed3f876c2cf5b900
Michael ZHANG <michael.zhang@epfl.ch>
2024-05-06 11:44:29 +0200

    Add STFT MUSIC variation

--- music.py
+++ music.py
@@ -25,27 +25,24 @@ def music(samples, n_sources):
     return signal_eigenvalues, signal_eigenvectors, noise_eigenvalues, noise_eigenvectors
 
 
-def covariance_in_frequency(samples, nperseg):
-    spectrums = []
+def compute_stfd(samples, nperseg, normalized_freq_range=[0, 0.5]):
+    sources_stft = []
     
     # Iterate over every microphone    
     for samples_channel in samples:
         freq, time_intervals, stft = scipy.signal.stft(samples_channel, nperseg=nperseg, return_onesided=True)
-                
-        # Average over every time interval
-        power_in_freq = stft.mean(axis=1)
         
-        spectrums.append(power_in_freq)
+        sources_stft.append(stft)
+        
+    sources_stft = np.array(sources_stft).transpose((1, 2, 0))
     
-    spectrums = np.array(spectrums).T # Matrix of size number_of_freq * number_of_mics
+    print(sources_stft.shape)
+    stfd_matrices = np.einsum('ijk,ijl->ijkl', sources_stft, sources_stft.conj())
+    print(stfd_matrices.shape)
     
-    # ChatGPT stuff, hmmmm
-    print(spectrums.shape)
-    covariance_matrices = np.einsum('ij,ik->ijk', spectrums, spectrums.conj())
-    print("Covariance matrices shape:", covariance_matrices.shape)
-    
-    return covariance_matrices
-
+    stfd = stfd_matrices.mean(axis=(0, 1))
+    print(stfd.shape)
+    return stfd
 
 def music_with_frequency(samples, n_sources, fs, mics_coords, segment_duration=0.5, freq_range=None):
     if freq_range is None:
@@ -57,34 +54,27 @@ def music_with_frequency(samples, n_sources, fs, mics_coords, segment_duration=0
         
     samples = (samples.T - samples.mean(axis=1).T).T
     
-    covariance_matrices = covariance_in_frequency(samples, nperseg)
+    stfd = compute_stfd(samples, nperseg)
     
-    eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrices)
+    eigenvalues, eigenvectors = np.linalg.eigh(stfd)
     
     # Decompose signal and noise
     # Assumption : the noise has the smallest eigenvalues
-    signal_eigenvalues, signal_eigenvectors = eigenvalues[:, -n_sources :], eigenvectors[:, :, -n_sources :]
-    noise_eigenvalues, noise_eigenvectors = eigenvalues[:, : -n_sources], eigenvectors[:, :, : -n_sources]
+    signal_eigenvalues, signal_eigenvectors = eigenvalues[-n_sources :], eigenvectors[:, -n_sources :]
+    noise_eigenvalues, noise_eigenvectors = eigenvalues[: -n_sources], eigenvectors[:, : -n_sources]
     
+    print("Shape of noise eigenvectors:", noise_eigenvectors.shape)
     
-    selected_freq_ids = (freq_range * nperseg) / (2 * fs)
-    print(selected_freq_ids)
-    spectrums = []
-    for freq_id in range(int(selected_freq_ids[0]), int(selected_freq_ids[1])):
-        spectrums.append(
-            general_spectrum_function(noise_eigenvectors[freq_id], mics_coords.T, (freq_id / nperseg) * fs)
-        )
-    
-    return spectrums
+    return general_spectrum_function(noise_eigenvectors, mics_coords.T, (np.mean(freq_range) / nperseg) * fs)
 
 
 def general_spectrum_function(noise_eigenvectors, mic_locations, main_frequency):
-    wavelength = SOUND_SPEED / (main_frequency + 1e-5)
+    wavelength = SOUND_SPEED / main_frequency
     
     M = noise_eigenvectors.shape[0]
     def helper(theta):
         a = np.array([np.cos(theta), np.sin(theta)])
-        atheta = np.exp(-1j * 2 * np.pi / wavelength * np.dot(mic_locations, a))
+        atheta = np.exp(1j * 2 * np.pi / wavelength * np.dot(mic_locations, a))
         temp = atheta.conj() @ noise_eigenvectors
         
         return 1 / np.linalg.norm(temp)**2
