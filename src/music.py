b92ae081f9f79cc8cd1c853c33c42065d6648c0b
Michael Zhang <57410827+MichelDucartier@users.noreply.github.com>
2024-05-22 20:58:46 +0200

    Add report (#2)
    * start of introduction/maths
    
    * want to pull master
    
    * general structure outlined
    
    * general structure outlined and python files documented
    
    * added proof of formula for non-alignes microphones
    
    * first draft of report
    
    * Add explanation + average over frequencies for STFT
    
    * Fix i am stupid
    
    * Add another limitation for MUSIC
    
    * Commit for first submission
    
    * draft of plots with awful code -.-
    
    ---------
    
    Co-authored-by: Sophie Strebel <sophiestrebel@Sophies-Air.sunrise.box>
    Co-authored-by: Sophie Strebel <sophiestrebel@Sophies-MacBook-Air.local>

--- src/music.py
+++ src/music.py
@@ -7,6 +7,21 @@ import scipy
 SOUND_SPEED = 343
     
 def music(samples, n_sources, mic_coords, main_frequency, correlated=False):
+    """
+    Computes and returns the estimated spatial spectrum by computing the eigendecomposition of the covariance matrix
+    of the samples
+
+    Parameters:
+    samples : matrix of shape (number of microphones) x (number of times sampled) holding all the information about
+              the received signals of each microphone
+    n_sources : number of sources
+    mic_coords : matrix of shape 2 x (number of microphones) containing coordinates of each microphone
+    main_frequency: main frequency
+    correlated : whether or not the signals sent by the sources are correlated
+
+    Returns:
+    The estimated spatial spectrum
+    """
     M = len(samples)
     
     samples = (samples.T - samples.mean(axis=1).T).T
@@ -30,76 +45,25 @@ def music(samples, n_sources, mic_coords, main_frequency, correlated=False):
     return general_spectrum_function(noise_eigenvectors, mic_coords.T, main_frequency)
 
 
-def compute_stfd(samples, nperseg, normalized_freq_range):
-    sources_stft = []
-    
-    # Iterate over every microphone    
-    for samples_channel in samples:
-        freq, time_intervals, stft = scipy.signal.stft(samples_channel, nperseg=nperseg, return_onesided=True)
-        
-        low_freq_mask = (normalized_freq_range[0] <= freq) & (freq <= normalized_freq_range[1])        
-        selected_freq_id = np.argwhere(low_freq_mask).flatten()
-        stft = stft[selected_freq_id, :]
-        
-        sources_stft.append(stft)
-        
-
-    sources_stft = np.array(sources_stft).transpose((1, 2, 0))
-    
-    print("Shape of sources STFT:", sources_stft.shape)
-    stfd_matrices = np.einsum('ijk,ijl->ijkl', sources_stft.conj(), sources_stft)
-    
-    stfd = stfd_matrices.mean(axis=(0, 1))
-    print("STFD shape:", stfd.shape)
-    
-    return stfd
-
-
-def music_with_frequency(samples, n_sources, fs, mics_coords, segment_duration=None, freq_range=None,
-                         correlated=False):
-    if freq_range is None:
-        freq_range = [0, fs]
-
-    if segment_duration is None:
-        nperseg = len(samples)
-    else:
-        # Number of samples to span the segment duration
-        nperseg = segment_duration * fs
-
-    freq_range = np.array(freq_range)
-    normalized_freq_range = freq_range / fs
-    
-    
-    samples = (samples.T - samples.mean(axis=1).T).T
-    stfd = compute_stfd(samples, nperseg, normalized_freq_range)
-
-    if correlated:
-        J = np.flip(np.eye(mics_coords.shape[1]), axis=1)
-        stfd_flipped = compute_stfd(J @ samples.conj(), nperseg, normalized_freq_range)
-        stfd = (stfd + stfd_flipped) / 2
-    
-    eigenvalues, eigenvectors = np.linalg.eigh(stfd)
-    
-    # Decompose signal and noise
-    # Assumption : the noise has the smallest eigenvalues
-    signal_eigenvalues, signal_eigenvectors = eigenvalues[-n_sources :], eigenvectors[:, -n_sources :]
-    noise_eigenvalues, noise_eigenvectors = eigenvalues[: -n_sources], eigenvectors[:, : -n_sources]
-    
-    print("Shape of noise eigenvectors:", noise_eigenvectors.shape)
-    
-    # Assume main frequency is the middle of the frequency range
-    main_frequency = (np.mean(freq_range) / nperseg) * fs
-
-    return general_spectrum_function(noise_eigenvectors, mics_coords.T, main_frequency)
-
-
 def general_spectrum_function(noise_eigenvectors, mic_locations, main_frequency):
+    """
+    Computes and returns the a function representing the estimated spatial spectrum to be evaluated at values of theta
+    using the noise eigenvectors of the covariance matrix of the samples
+
+    Parameters:
+    noise_eigenvectors : noise eigenvectors in columns of a matrix
+    mic_locations : coordinates of the microphones
+    main_frequency : main frequency
+    
+    Returns:
+    The estimated spatial spectrum as a function of theta
+    """
     wavelength = SOUND_SPEED / main_frequency
     
     def helper(theta):
         a = np.array([np.cos(theta), np.sin(theta)])
         atheta = np.exp(-2j * np.pi / wavelength * np.dot(mic_locations, a))
-        temp = atheta.conj() @ noise_eigenvectors
+        temp = atheta.conj() @ noise_eigenvectors #noise_eigenvectors.T.conj() @ atheta also works
         
         return 1 / np.linalg.norm(temp)**2
     
@@ -107,6 +71,18 @@ def general_spectrum_function(noise_eigenvectors, mic_locations, main_frequency)
 
 
 def extract_frequencies(spectrum, n_sources, input_range, resolution=10000):
+    """
+    Extracts the frequencies at which the spectrum function has its peaks
+
+    Parameters:
+    spectrum : the spatial spectrum, function of theta
+    n_sources: number of sources
+    input_range : input range of theta
+    resolution : number of points to evaluate the spectrum function
+    
+    Returns:
+    Frequencies at which the spectrum function peaks
+    """
     X = np.linspace(input_range[0], input_range[1], resolution)
     Y = np.array([spectrum(x) for x in X])
     
@@ -118,16 +94,3 @@ def extract_frequencies(spectrum, n_sources, input_range, resolution=10000):
     
     return (estimated_freq / resolution) * (input_range[1] - input_range[0]) + input_range[0]
 
-
-@DeprecationWarning
-def mic_array_spectrum_function(noise_eigenvectors, mic_distance, wavelength=1):
-    M = noise_eigenvectors.shape[0]
-    def helper(sin_value):
-        phi = 2 * np.pi * mic_distance * sin_value / wavelength
-        x = [np.exp(-1j*phi)]
-        a = np.vander(x, M, increasing=True)
-        temp = a.conj() @ noise_eigenvectors
-        
-        return 1 / np.linalg.norm(temp)**2
-    
-    return helper
--- src/simulated_signal.py
+++ src/simulated_signal.py
@@ -1,7 +1,17 @@
 import numpy as np
 from music import SOUND_SPEED
 
-def narrowband_signal(k, w0):    
+def narrowband_signal(k, w0):
+    """
+    Creates and returns a function (representing a signal) to be evaluated at a time step t with parameters k and w0
+
+    Parameters:
+    k : index of the source sending the signal
+    w0 : main frequency
+
+    Returns:
+    A deterministic function of k and t, to be evaluated at some t
+    """
     def helper(t):
         # Use a hash as a seed to ensure that if (k1, t1) = (k2, t2), then we give the exact result
         rng = np.random.RandomState(hash((k, t)) & 0xffffffff)
@@ -10,6 +20,22 @@ def narrowband_signal(k, w0):
 
 
 def received_signal(thetas, narrowband_signals, mic_index, main_frequency, mic_distance=0.2, noise_var=0):
+    """
+    Creates and returns a function (representing a signal) to be evaluated at a time step t representing the received signal
+    of the microphone at the given index assuming the coordinates of the m'th microphone are (0, mic_distance * (m-1)),
+    sum of all sources and additive white Gaussian noise
+
+    Parameters:
+    thetas : angles of the sources
+    narrowband_signals : signals sent by the sources
+    mic_index : index of the microphone
+    main_frequency : main frequency
+    mic_distance : distance between microphones assuming they are aligned
+    noise_var : variance/power of the additive white Gaussian noise
+
+    Returns:
+    A function to be evaluated at time step t representing the received signal of the mic_index'th microphone at that time
+    """
     wavelength = SOUND_SPEED / main_frequency
     
     def helper(t):
